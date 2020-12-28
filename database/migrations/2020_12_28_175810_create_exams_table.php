62f0a700809a0ff3df1171a4072f369ddfc7945e
kareem-torky <kareemmfouad.ece@gmail.com>
2020-12-28 21:06:20 +0200

    migrations initialize

---
+++ database/migrations/2020_12_28_175810_create_exams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('skill_id')->constrained();
+            $table->string('name', 255);
+            $table->string('img', 50);
+            $table->text('desc');
+            $table->tinyInteger('difficulty');
+            $table->smallInteger('duration_mins');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+}
